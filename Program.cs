a7ede0bc5ab42bd80c94a8c4649e76529ae8ca86
Caesar Kabalan <Dandelock@gmail.com>
2011-12-07 03:35:32 +0000

    git-svn-id: file:///content/svn/csharp/redditcartographer@1 6c1bafe5-b637-430b-96d9-2b642778e4d6

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedditCartographer {
+	class Program {
+		static void Main(string[] args) {
+			PrintWelcome();
+		}
+
+		private static void PrintWelcome() {
+			Console.WriteLine("Welcome to the Reddit Cartographer!");
+			Console.WriteLine("-----------------------------------");
+		}
+
+	}
+}
